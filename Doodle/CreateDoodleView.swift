2300292ba60ad98af9765abc4e4c7aefbb689275
Ganesh Sarma K <ganeshsarma16@gmail.com>
2025-04-02 22:13:41 +0530

    Added Colour Picker and Reset button for Canvas

--- Doodle/CreateDoodleView.swift
+++ Doodle/CreateDoodleView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct InputView: View {
+struct CreateDoodleView: View {
     
     @Environment(\.modelContext) private var modelContext
     
@@ -15,16 +15,32 @@ struct InputView: View {
     
     @State private var textFieldInputText: String = ""
     
+    @State private var selectedColor: Color = .random
+    
     @Binding var path: [String]
     
     var body: some View {
-       
+        
         VStack {
             TextField("Enter text to add", text: $textFieldInputText)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .frame(maxWidth: 300)
                 .padding()
                 .shadow(color: colorScheme == .dark ? .white : .gray, radius: 2, x: 0.5, y: 0.5)
+            
+            
+            HStack{
+                Spacer()
+                
+                Text("Pick a colour")
+                    .padding()
+                
+                ColorPicker("Pick a Color", selection: $selectedColor)
+                    .padding()
+                    .labelsHidden()
+                
+                Spacer()
+            }
             
             Button(action: addNewTextItem,
                    label: {
@@ -39,7 +55,7 @@ struct InputView: View {
             .disabled(textFieldInputText.isEmpty)
             
         }
-        .navigationTitle(Text("Input"))
+        .navigationTitle(Text("Create Doodle"))
         .toolbar {
             ToolbarItem(placement: .principal) { // Centers & styles title
                 Text("Input")
@@ -53,13 +69,24 @@ struct InputView: View {
     func addNewTextItem() {
         var newItem = DoodleItem(
             text: textFieldInputText,
-            colour: .random,
+            colour: selectedColor,
             location: CGPoint(x: .randomWidth + 30, y: .randomHeight),
             scaleValue: 1.0,
             rotation: .zero
         )
         
         modelContext.insert(newItem)
+        
+        for index in 1...30 {
+            let newItem = DoodleItem(
+                text: "Item \(index)",
+                colour: .random,
+                location: CGPoint(x: .randomWidth + 30, y: .randomHeight),
+                scaleValue: 1.0,
+                rotation: .zero
+            )
+            modelContext.insert(newItem)
+        }
         
         do {
             try modelContext.save()
@@ -68,6 +95,7 @@ struct InputView: View {
         }
         path.removeLast()
     }
+    
 }
 
 
